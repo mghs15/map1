cfd02e341ae78b3a63bd337e2b6b1ac88a534f93
mghs <40787295+mghs15@users.noreply.github.com>
2018-11-11 20:49:22 +0900

    Update xyz/test1111/style.js

--- xyz/test1111/style.js
+++ xyz/test1111/style.js
@@ -3,7 +3,8 @@
 geojsonOptions:{
   pointToLayer: function (feature, latlng) {
     var imgel = "<img src='" + feature.properties['_iconUrl'] + "' style='margin:0px; width:13px; height:13px;' class='leaflet-marker-icon'></img>";
-    var spanel = "<span style='position:absolute; left:13px; top:-4px;'>" + feature.properties["name"] + feature.properties["number"] + "</span>";
+    var spanel = "<span style='position:absolute; left:13px; top:-4px;'>" + feature.properties["name"] + "</span>";
+    var spanel2 = "<span style='position:absolute; left:13px; top:-4px;'>" +  feature.properties["number"] + "</span>";
     var htmlel = "";
     var z = GSI.GLOBALS.map.getZoom();
     if(z < 7) {
@@ -12,7 +13,7 @@ geojsonOptions:{
     if(!feature.properties["name"]) {
     if(z < 12) {
       imgel = "";
-      spanel = "";
+      spanel2 = "";
     }}
     htmlel = imgel + spanel + spanel2;
     var myIcon = L.divIcon({html:htmlel, className:"gsi-div-icon", iconAnchor: [7, 7], iconSize: [13, 13]});
@@ -26,15 +27,31 @@ geojsonOptions:{
     for(name in feature.properties) {
       if(!name.match(/^_/)){
         if(name=="name"){
-          s += "<tr><th colspan='2' style='font-size:14px; font-weight:bold; color:#000000;'>" + feature.properties[name] + feature.properties[number] + "</th></tr>";
+          s += "<tr><th colspan='2' style='font-size:14px; font-weight:bold; color:#000000;'>" + feature.properties[name] + "</th></tr>";
         }else if(name=="description"){
-          s += "<tr><td colspan='2' style='font-size:14px; color:#000000;'>" + feature.properties[name] + feature.properties[number] + "</td></tr>";
+          s += "<tr><td colspan='2' style='font-size:14px; color:#000000;'>" + feature.properties[name] + "</td></tr>";
         }
         }else if(name=="number"){
-          s += "<tr><td colspan='2' style='font-size:14px; color:#000000;'>" + feature.properties[name] + feature.properties[number] + "</td></tr>";
+          s += "<tr><td colspan='2' style='font-size:14px; color:#000000;'>" + feature.properties[name] + "</td></tr>";
         }
         else{
           s += "<tr><td style='vertical-align:top; font-size:14px; color:#0000ff;'>" + name + "</td>"
+               + "<td style='font-size:14px; color:#000000;'>" + feature.properties[name] + "</td></tr>";
+        }
+      }
+    }
+    for(number in feature.properties) {
+      if(!number.match(/^_/)){
+        if(name=="number"){
+          s += "<tr><th colspan='2' style='font-size:14px; font-weight:bold; color:#000000;'>" + feature.properties[number] + "</th></tr>";
+        }else if(name=="description"){
+          s += "<tr><td colspan='2' style='font-size:14px; color:#000000;'>" +  feature.properties[number] + "</td></tr>";
+        }
+        }else if(name=="number"){
+          s += "<tr><td colspan='2' style='font-size:14px; color:#000000;'>" + feature.properties[number] + "</td></tr>";
+        }
+        else{
+          s += "<tr><td style='vertical-align:top; font-size:14px; color:#0000ff;'>" + number + "</td>"
                + "<td style='font-size:14px; color:#000000;'>" + feature.properties[number] + "</td></tr>";
         }
       }
