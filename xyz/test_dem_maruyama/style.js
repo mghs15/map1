112f610671fc147a15b75478c33955913a747d95
mghs <40787295+mghs15@users.noreply.github.com>
2019-05-23 22:04:08 +0900

    Update layers.txt

---
+++ xyz/test_dem_maruyama/style.js
@@ -0,0 +1,42 @@
+{
+options:
+{
+  attribution: '基盤地図情報ベクトルタイル',minZoom:18,maxNativeZoom: 18,maxZoom: 24
+},
+geojsonOptions:
+{
+    pointToLayer: function(feature, latlng) {
+        var client = feature.properties['_client'];
+        if(client && client == "gsi.3d"){
+            var mark = L.circleMarker(latlng, { weight : 0, color : "#000000", opacity : 0.0, fillColor : "#000000", fillOpacity : 0.3 });
+            mark.setRadius(5);
+            return mark;
+        }
+        else{
+            return L.marker(latlng, {
+            icon      : L.divIcon({
+            iconAnchor: [5,5],
+            className : "gsi-div-icon",
+            html: '<div style=\"'
+            +'width: 10px;'
+            +'height: 10px;'
+            +'background-color: rgba(0, 0, 0, 0.3);'
+            +'-webkit-border-radius: 10px;'
+            +'-moz-border-radius: 10px;'
+            +'border-radius: 10px;'
+            +'\">' + 
+            '</div>'
+            })
+            });
+        }
+    },
+   onEachFeature: function (feature, layer) {
+    var s = ''
+    for(var n in feature.properties) {
+      s += "<a style='font-size: 12px;color:#000;'>" + n + "：" + feature.properties[n] + "</a><br>";
+    }
+    layer.bindPopup(s);
+   }
+}
+}
+
