659a45914b8b389f6126e97c8ccee1ebcb76172d
mghs <40787295+mghs15@users.noreply.github.com>
2019-05-23 23:42:40 +0900

    Update style.js

--- xyz/test_dem_maruyama/style.js
+++ xyz/test_dem_maruyama/style.js
@@ -7,8 +7,8 @@ geojsonOptions:
 {
     pointToLayer: function(feature, latlng) {
         var client = feature.properties['_client'];
-        var height_ori = Math.round(feature.properties['alti'] + 100000);
-        var height_grad = "#" + height_ori;
+        var height_ori = Math.round(feature.properties['alti'] + 1000);
+        var height_grad = "#" + "0" + height_ori + "0";
         if(client && client == "gsi.3d"){
             var mark = L.circleMarker(latlng, { weight : 0, color : height_grad, opacity : 0.0, fillColor : "#000000", fillOpacity : 0.3 });
             mark.setRadius(5);
